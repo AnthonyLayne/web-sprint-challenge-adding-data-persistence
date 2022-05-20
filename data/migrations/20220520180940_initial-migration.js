3c348d11e27693473ce0610267be71b18c03a24b
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-20 15:35:52 -0400

    added migrations

--- data/migrations/20220520180940_initial-migration.js
+++ data/migrations/20220520180940_initial-migration.js
@@ -3,9 +3,51 @@
  * @returns { Promise<void> }
  */
 exports.up = async function (knex) {
-  await knex.schema.createTable("projects", (table) => {
-    table.increments("project_id");
-  });
+  await knex.schema
+    .createTable("projects", (table) => {
+      table.increments("project_id");
+      table.string("project_name", 50).notNullable();
+      table.string("project_description", 200);
+      table.boolean("project_completed").notNullable().defaultTo(0);
+    })
+    .createTable("resources", (table) => {
+      table.increments("resource_id");
+      table.string("resource_name", 50).notNullable().unique();
+      table.string("resource_description", 200);
+    })
+    .createTable("tasks", (table) => {
+      table.increments("task_id");
+      table.string("task_description", 120).notNullable();
+      table.string("task_notes", 200);
+      table.boolean("task_completed").notNullable().defaultTo(0);
+      table
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    })
+    .createTable("project_resources", (table) => {
+      table.increments("project_resources_id");
+      table
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      table
+        .integer("resources_id")
+        .unsigned()
+        .notNullable()
+        .references("resources_id")
+        .inTable("resources")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    });
 };
 
 /**
@@ -13,5 +55,9 @@ exports.up = async function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = async function (knex) {
-  await knex.schema;
+  await knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
 };

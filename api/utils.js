b7f8dc2040d16479f7e302a7e24c7433e5f100d4
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-21 03:43:28 -0400

    fix utils

--- api/utils.js
+++ api/utils.js
@@ -1,7 +1,7 @@
 const validateKeys = (validKeys, obj) => {
   const objKeys = Object.keys(obj);
 
-  return validKeys.every((k) => objKeys.includes(k)) && objKeys.every((k) => validKeys.includes(k));
+  return validKeys.every((k) => objKeys.includes(k));
 };
 
 module.exports = {
